f553c1550c60b78f7c17a620ba76800707f8e089
Kai <azazelwoodwind11@gmail.com>
2023-05-10 05:31:44 +0200

    fixed EVERYTHING

--- serverOld.js
+++ serverOld.js
@@ -16,4 +16,4 @@ app.get("*", (req, res) => {
 
 app.listen(process.env.PORT || 8080);
 
-console.log(`listening on port ${process.env.PORT || 8080}`);
+console.log(`listening on  port ${process.env.PORT || 8080}`);
