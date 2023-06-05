002a6a71f33e2183a8de2b02501bec46363c731a
Kai <azazelwoodwind11@gmail.com>
2023-06-05 23:11:47 +0200

    correct the ports again?

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(8000);
+app.listen(process.env.PORT || 8000);
 
-console.log(`listening on  port ${8000}`);
+console.log(`listening on  port ${process.env.PORT || 8000}`);
