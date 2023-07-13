5cc1f1dbb0256c9c29c8037f67f0765dbad91549
Kai <azazelwoodwind11@gmail.com>
2023-07-13 04:56:05 +0200

    change waiting list api routes

--- src/hooks/useXAvatar/Pulse.jsx
+++ src/hooks/useXAvatar/Pulse.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
 import FillParent from "../../styles/containers/FillParent";
+import React from "react";
 
 const animation = {
     boxShadow: [
