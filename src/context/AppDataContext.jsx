77f9467944de2c4909af8fa4c86f17843347248b
Kai <azazelwoodwind11@gmail.com>
2023-12-29 18:27:07 +0100

    refactor: convert Promise.all to Promise.allSettled

--- src/context/AppDataContext.jsx
+++ src/context/AppDataContext.jsx
@@ -4,6 +4,7 @@ import EducationLevelsAPI from "../api/EducationLevelAPI";
 import ExamBoardAPI from "../api/ExamBoardAPI";
 import UsagePlanAPI from "../api/UsagePlansAPI";
 import Loading from "@/components/common/feedback/Loading";
+import { useNotification } from "./NotificationContext";
 
 /**
  * Context for providing application-wide data.
@@ -28,25 +29,41 @@ export function AppDataContextProvider({ children }) {
     const [fetching, setFetching] = React.useState(true);
     const [usagePlans, setUsagePlans] = React.useState({});
 
+    const { sendNotification } = useNotification();
+
     // Fetch and set data for subjects, education levels, exam boards, and usage plans on component mount
     React.useEffect(() => {
         // use Promise.allSettled
-        Promise.all([
-            SubjectsAPI.getAll().then(setSubjectOptions),
-            EducationLevelsAPI.getAll().then(setEducationLevels),
-            ExamBoardAPI.getAll().then(setExamBoards),
-            UsagePlanAPI.getAll()
-                .then(res => {
-                    const temp = {};
-                    res.forEach(usagePlan => {
-                        temp[usagePlan.plan] = usagePlan.max_daily_tokens;
+        const fetch = async () => {
+            // const [subjects, educationLevels, examBoards, usagePlans]
+            const results = await Promise.allSettled([
+                SubjectsAPI.getAll(),
+                EducationLevelsAPI.getAll(),
+                ExamBoardAPI.getAll(),
+                UsagePlanAPI.getAll(),
+            ]);
+
+            const pairs = [
+                setSubjectOptions,
+                setEducationLevels,
+                setExamBoards,
+                setUsagePlans,
+            ].map((setter, i) => [results[i], setter]);
+            for (const [result, setter] of pairs) {
+                if (result.status === "rejected") {
+                    console.error(result.reason);
+                    sendNotification({
+                        type: "error",
+                        label: `Error fetching data: ${result.reason}`,
                     });
-                    return temp;
-                })
-                .then(setUsagePlans),
-        ]).then(() => {
+                    continue;
+                }
+                setter(result.value);
+            }
             setFetching(false);
-        });
+        };
+
+        fetch();
     }, []);
 
     return (
