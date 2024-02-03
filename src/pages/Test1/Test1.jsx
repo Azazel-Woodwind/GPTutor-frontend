5aab699a1a6ea7eaddaff07f0644543578f3cc86
Kai <kaistrachan11@gmail.com>
2024-02-03 19:20:06 +0100

    change vite js

--- src/pages/Test1/Test1.jsx
+++ src/pages/Test1/Test1.jsx
@@ -6,11 +6,71 @@ import useFillingButton from "@/hooks/useFillingButton";
 import { SocketContext, SocketContextProvider } from "@/context/SocketContext";
 import Textfield from "@/components/common/input/Textfield";
 import FillingButton from "./FillingButton";
+import Slider from "@/components/common/input/Slider";
+import Button from "@/components/common/input/Button";
+
+const TestButton = styled(motion.button)`
+    /* width: 1px; */
+    padding: 1rem 2rem;
+    border: 2px solid black;
+    border-radius: 0.5rem;
+    background-color: white;
+    color: black;
+    font-size: 1rem;
+    font-weight: 600;
+    cursor: pointer;
+    outline: none;
+`;
+
+const variants = {
+    idle: {
+        rotate: [0, 0, 8, -8, 0, 0, 0],
+        scale: [1, 1.1, 1.1, 1.1, 1.1, 1.1, 1],
+        transition: {
+            repeat: Infinity,
+            duration: 1,
+            delay: 2,
+            repeatDelay: 5,
+        },
+    },
+    hover: {
+        scale: 1.05,
+        rotate: 0,
+        transition: {
+            scale: {
+                duration: 0.1,
+            },
+        },
+    },
+    tap: {
+        scale: 0.95,
+        rotate: 0,
+        transition: { duration: 0.1 },
+    },
+};
 
 function Main() {
+    const [state, setState] = React.useState();
+
     return (
         <CenteredColumn fillparent>
-            <FillingButton />
+            <Button
+                gestures={false}
+                style={{ borderRadius: "3rem" }}
+                size="xl"
+                animate="idle"
+                variants={variants}
+                whileHover="hover"
+                whileTap="tap">
+                Join the Waiting List
+            </Button>
+            <Slider
+                // min={0}
+                // max={1}
+                // step={0.01}
+                // value={0.5}
+                value={state}
+                onChange={setState}></Slider>
         </CenteredColumn>
     );
 }
